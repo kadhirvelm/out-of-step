0c7235779a27e62bccf0cd9b9c39e9577369c835
Kadhir Mani <kadhirvelm@gmail.com>
2020-12-16 23:26:33 -0800

    Add second stock

--- packages/backend/src/cronJobs/stocks/stockPricerPlugins.ts
+++ packages/backend/src/cronJobs/stocks/stockPricerPlugins.ts
@@ -3,6 +3,6 @@ import { priceStabilityEnterprises } from "./stockPlugins/stabilityEnterprises";
 import { IStockPricerPlugin } from "./types";
 
 export const STOCK_PRICER_PLUGINS: { [stockName: string]: IStockPricerPlugin } = {
-    // "Agri Cola Inc": priceAgriColaInc,
+    "Agri Cola Inc": priceAgriColaInc,
     "Stability Enterprises": priceStabilityEnterprises,
 };
