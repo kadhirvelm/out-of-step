29f45f0aa353cbf7597326c542032a9ac910fede
Kadhir Mani <kadhirvelm@gmail.com>
2020-12-18 12:53:24 -0800

    Fix tests by adding explicit timezone

--- packages/backend/src/cronJobs/market/__tests__/getNextTimeWithinMarketHours.test.ts
+++ packages/backend/src/cronJobs/market/__tests__/getNextTimeWithinMarketHours.test.ts
@@ -8,35 +8,35 @@ dayjs.extend(timezone);
 
 describe("can correctly increment the time", () => {
     it("can return a valid time", () => {
-        const currentDate = dayjs("2020-12-15 15:00:00", "America/Los_Angeles");
+        const currentDate = dayjs("2020-12-15 15:00:00 PST", "America/Los_Angeles");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(currentDate.valueOf());
     });
 
     it("can increment to the next day", () => {
-        const currentDate = dayjs("2020-12-15 22:00:00", "America/Los_Angeles");
+        const currentDate = dayjs("2020-12-15 22:00:00 PST", "America/Los_Angeles");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-16 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-16 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 
     it("can increment to the start of the market", () => {
-        const currentDate = dayjs("2020-12-15 03:00:00", "America/Los_Angeles");
+        const currentDate = dayjs("2020-12-15 03:00:00 PST", "America/Los_Angeles");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-15 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-15 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 
     it("can increment to the start of the next market day", () => {
-        const currentDate = dayjs("2020-12-18 21:00:00", "America/Los_Angeles");
+        const currentDate = dayjs("2020-12-18 21:00:00 PST", "America/Los_Angeles");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-21 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-21 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 
     it("can increment to the start of the next market day take 2", () => {
-        const currentDate = dayjs("2020-12-19 15:00:00", "America/Los_Angeles");
+        const currentDate = dayjs("2020-12-19 15:00:00 PST", "America/Los_Angeles");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-21 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-21 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 
@@ -48,14 +48,14 @@ describe("can correctly increment the time", () => {
     it("can increment to the start of the next market day in Europe/London", () => {
         const currentDate = dayjs("2020-12-19 05:00:00 GMT", "Europe/London");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-21 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-21 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 
     it("can increment to the start of the next market day take 2 in Europe/London", () => {
         const currentDate = dayjs("2020-12-19 23:00:00 GMT", "Europe/London");
         expect(getNextTimeWithinMarketHours(currentDate).valueOf()).toEqual(
-            dayjs("2020-12-21 06:00:00", "America/Los_Angeles").valueOf(),
+            dayjs("2020-12-21 06:00:00 PST", "America/Los_Angeles").valueOf(),
         );
     });
 });
