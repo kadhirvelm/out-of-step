501dae8a0812345427ec3c40427e8e7cad81aaf2
Kadhir Mani <kadhirvelm@gmail.com>
2020-12-17 23:20:01 -0800

    Fix some things

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -6,6 +6,7 @@ import { createServer } from "http";
 import { instantiateAllCronJobs } from "./cronJobs/instantiateAllCronJobs";
 import { configureAllRoutes } from "./routes/configureAllRoutes";
 import { configureSecurity } from "./security/configureSecurity";
+import { pingServerToPreventSleep } from "./utils/pingServerToPreventSleep";
 
 const app = express();
 const server = createServer(app);
@@ -24,11 +25,15 @@ if (ORIGIN !== undefined) {
     server.listen(PORT as number | undefined, ORIGIN, () => {
         // eslint-disable-next-line no-console
         console.log({ level: "info", message: `Server started, listening on http://${ORIGIN}:${PORT}` });
+
+        pingServerToPreventSleep();
     });
 } else {
     // PRODUCTION
     server.listen(PORT || 3000, () => {
         // eslint-disable-next-line no-console
         console.log({ level: "info", message: `Server started, listening on ${PORT}` });
+
+        pingServerToPreventSleep();
     });
 }
---
+++ packages/backend/src/utils/pingServerToPreventSleep.ts
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+const INTERVAL = 1000 * 60 * 25;
+
+export function pingServerToPreventSleep() {
+    setTimeout(async () => {
+        try {
+            await fetch(`${process.env.HOSTNAME}/`);
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error("Something went wrong trying to ping the server.", e);
+        } finally {
+            pingServerToPreventSleep();
+        }
+    }, INTERVAL);
+}
