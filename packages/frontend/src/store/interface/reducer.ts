e5f9947b0220b6db750fa1ee5f22912d1bacda4b
Kadhir Mani <kadhirvelm@gmail.com>
2020-12-26 13:57:03 -0800

    Add dark mode when the market is closed

--- packages/frontend/src/store/interface/reducer.ts
+++ packages/frontend/src/store/interface/reducer.ts
@@ -9,7 +9,19 @@ export interface IInterfaceState {
 }
 
 export const EMPTY_INTERFACE_STATE: IInterfaceState = {
-    viewStockWithLatestPrice: undefined,
+    viewStockWithLatestPrice: {
+        dollarValue: 40.43,
+        id: "30e0cc31-5a93-4a03-ac30-fba9003e91e2" as any,
+        name: "Agri Cola Inc",
+        previousPriceHistory: {
+            dollarValue: 43.13,
+            timestamp: "2020-12-25T04:55:33.000Z",
+        },
+        priceHistoryId: "97d6d6c4-74c5-4551-82c1-ac43ff0e836d" as any,
+        status: "AVAILABLE",
+        timestamp: "2020-12-26T20:49:57.000Z",
+        totalQuantity: 500000,
+    },
     viewTransactionsForStock: undefined,
     viewStockDetails: undefined,
 };
