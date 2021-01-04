f7fe22e6965dc47c859bc7cc92f27bd954ff0042
Kadhir <kadhirvelm@gmail.com>
2021-01-03 23:19:49 -0800

    Adjust things, add random value (#62)

--- packages/backend/src/cronJobs/stocks/utils/stabilizeDollar.ts
+++ packages/backend/src/cronJobs/stocks/utils/stabilizeDollar.ts
@@ -3,6 +3,11 @@ import { getNumberWithinRange } from "../../../utils/getNumberWithinRange";
 import { roundToNearestHundreth } from "../../../utils/roundToNearestHundreth";
 import { IStockPriceReturnType } from "../types";
 
+const RANDOMLY_ADJUST_ON_PERCENT_DIFFERENCE = 0.5;
+const RANDOMLY_ADJUST_BY_PERCENT_OF_ORIGINAL_PRICE = 3;
+
+const randomlyAssignPositiveOrNegative = () => (Math.random() >= 0.5 ? 1 : -1);
+
 export function stabilizeNextDollarValue(
     nextDollarValue: IStockPriceReturnType<{}>,
     stock: IStock,
@@ -24,13 +29,27 @@ export function stabilizeNextDollarValue(
     const adjustedChangeInValueByPercentOwnership = roundToNearestHundreth(1 - 0.9 * totalOwnedStockPercent);
     const changeInValue = stabilizedDollarValue - safeNumberToStabilizeAgainst;
 
-    // Account for percent ownership by reducing total change by up to 50% when ownership is at 100%
-    const stabilizedDollarPrice =
-        safeNumberToStabilizeAgainst + changeInValue * adjustedChangeInValueByPercentOwnership;
+    // Account for percent ownership by reducing total change by up to 90% when ownership is at 100%
+    let changeInValueAdjustedByPercentOwnership = changeInValue * adjustedChangeInValueByPercentOwnership;
+    let didRandomlyAssign = false;
+
+    const percentChange =
+        Math.round(Math.abs(changeInValueAdjustedByPercentOwnership / safeNumberToStabilizeAgainst) * 1000) / 1000;
+
+    if (percentChange <= RANDOMLY_ADJUST_ON_PERCENT_DIFFERENCE / 100) {
+        changeInValueAdjustedByPercentOwnership =
+            Math.random() *
+            ((safeNumberToStabilizeAgainst * RANDOMLY_ADJUST_BY_PERCENT_OF_ORIGINAL_PRICE) / 100) *
+            randomlyAssignPositiveOrNegative();
+        didRandomlyAssign = true;
+    }
+
+    const stabilizedDollarPrice = safeNumberToStabilizeAgainst + changeInValueAdjustedByPercentOwnership;
 
     const calculationNotes = JSON.stringify({
         ...nextDollarValue.calculationNotes,
         adjustedChangeByPercentOwnership: adjustedChangeInValueByPercentOwnership,
+        didRandomlyAssign,
     });
 
     return {
