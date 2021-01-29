a0ee35c52451c85fd38141fa24f5f23b94591c25
Kadhir Mani <kadhirvelm@gmail.com>
2021-01-29 08:12:59 -0800

    Close market this weekend

---
+++ packages/backend/src/cronJobs/stocks/stockPlugins/REPLACE_ME.ts
@@ -0,0 +1,4 @@
+// https://api.fda.gov/drug/enforcement.json?search=report_date:[20210101+TO+20210110]&limit=0
+// https://api.fda.gov/device/enforcement.json?search=report_date:[20210101+TO+20210110]&limit=0
+// https://api.fda.gov/food/enforcement.json?search=report_date:[20210101+TO+20210110]&limit=0
+// https://api.exchangeratesapi.io/latest
